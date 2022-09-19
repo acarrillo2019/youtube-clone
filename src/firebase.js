79e6aad7315a9dcdb979250fd9d1dd6d9e57bb0c
Alfredo Carrillo <alric21@aol.com>
2022-09-19 09:13:31 -0700

    install material

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDMQpdFIey7HhT8nunfCA-SuftT-3Pleao",
+    authDomain: "clone-22235.firebaseapp.com",
+    projectId: "clone-22235",
+    storageBucket: "clone-22235.appspot.com",
+    messagingSenderId: "333677684357",
+    appId: "1:333677684357:web:84088b405ce7aaa5c9ebec",
+    measurementId: "G-57QJQKJEHE"
+  };
