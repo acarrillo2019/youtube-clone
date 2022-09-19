6cf52e89692ef80e7ee41c404967869d20a4e7ba
Alfredo Carrillo <alric21@aol.com>
2022-09-19 09:35:17 -0700

    added icons

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
+import SearchIcon from '@mui/icons-material/Search';
+import VideoCallIcon from '@mui/icons-material/VideoCall';
 
 function Header() {
   return (
     <div className='header'>
         <h1>Header</h1>
+        <MenuSharpIcon />
+        <img 
+            className='header_logo'
+            src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+            alt=''
+        />
+        <input type="text" />
+        <SearchIcon />
+        <VideoCallIcon />
+
+
     </div>
   )
 }
