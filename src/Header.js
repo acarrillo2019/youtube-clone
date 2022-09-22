07f711d7d54e936ad3d20631e19b83199a8e1659
Alfredo Carrillo <alric21@aol.com>
2022-09-22 10:06:24 -0700

    added search and home routes

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import MenuSharpIcon from '@mui/icons-material/MenuSharp';
 import SearchIcon from '@mui/icons-material/Search';
@@ -6,34 +6,44 @@ import VideoCallIcon from '@mui/icons-material/VideoCall';
 import AppsIcon from '@mui/icons-material/Apps';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import { Avatar } from '@mui/material';
+import {Link} from "react-router-dom";
 
 function Header() {
-  return (
-    <div className='header'>
-        <div className='header__left'>
-            <MenuSharpIcon />
-            <img 
-                className='header__logo'
-                src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
-                alt=''
-            />
-        </div>
+    const [inputSearch, setInputSearch] = useState("");
 
-        <div className='header__input'>
-            <input placeholder="Search" type="text" />
-            <SearchIcon className='header__inputButton'/>
-        </div>
+    return (
+        <div className='header'>
+            <div className='header__left'>
+                <MenuSharpIcon />
+                <img 
+                    className='header__logo'
+                    src="https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg"
+                    alt=''
+                />
+            </div>
 
-        <div className='header__icons'>
-            <VideoCallIcon className='header__icon'/>
-            <AppsIcon className='header__icon'/>
-            <NotificationsIcon className='header__icon'/>
-            <Avatar
-                alt="Profile Pic"
-                src="https://ff-storage-p01-storage.filmfreeway.com/press_kits/headshots/002/113/444/original/f09182d9ec-headshot.jpg?1662997322"
-            />
+            <div className='header__input'>
+                <input
+                    onChange={e => setInputSearch(e.target.value)} 
+                    value={inputSearch}
+                    placeholder="Search" 
+                    type="text" 
+                />
+                <Link to={`/search/${inputSearch}`}>
+                    <SearchIcon className='header__inputButton'/>
+                </Link>
+            </div>
+
+            <div className='header__icons'>
+                <VideoCallIcon className='header__icon'/>
+                <AppsIcon className='header__icon'/>
+                <NotificationsIcon className='header__icon'/>
+                <Avatar
+                    alt="Profile Pic"
+                    src="https://ff-storage-p01-storage.filmfreeway.com/press_kits/headshots/002/113/444/original/f09182d9ec-headshot.jpg?1662997322"
+                />
+            </div>
         </div>
-    </div>
   )
 }
 
--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import VideoCard from './VideoCard';
 import "./RecommendedVideos.css";
 
 function RecommendedVideos() {
   return (
     <div className='recommendedVideos'>
         <h2>Recommended</h2>
+        <div className='recommendedVideos__video'>
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+            <VideoCard />
+        </div>
     </div>
   )
 }
